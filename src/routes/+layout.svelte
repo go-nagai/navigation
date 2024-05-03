a80cff60b66c466371162bef7c74adcac18a78aa
Go-Nagai <mikael@dilife.fr>
2024-05-03 10:05:37 +0200

    Last changes to make form save state

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+</script>
+
+<svelte:head>
+    <title>Navigation</title>
+</svelte:head>
+
+<slot></slot>
+
+<style>
+    :global(*) {
+        font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,63 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import { onMount } from 'svelte';
+    import { goto, afterNavigate } from '$app/navigation';
+
+    export let data;
+    let formdata = {};
+
+    export const snapshot = {
+		capture: () => formdata,
+		restore: (value) => formdata = value
+	};
+
+    const getState = () => {
+        const params = new URLSearchParams(window.location.search);
+        if (params.has('state')) {
+            return params.get('state');
+        } else {
+            return 'default';
+        }
+    }
+
+    onMount(() => {
+        data.state = getState();
+    });
+
+    afterNavigate(() => {
+        data.state = getState();
+    })
+
+    const changeState = (newState) => {
+        goto(`?state=${newState}`);
+    };
+</script>
+
+<h1>Page {(data.state === 'results') ? 2 : 1}</h1>
+
+<div>
+    <p>Current State: {data?.state}</p>
+    {#if data.state === 'results'}
+         <!-- content here -->
+         {JSON.stringify(formdata)}
+         <button on:click={() => changeState('form')}>Back</button>
+    {:else}
+         <!-- else content here -->
+         <form>
+            <div class="mb-3">
+                <label for="name" class="form-label">Name</label>
+                <input
+                    type="text"
+                    class="form-control"
+                    name="name"
+                    id="name"
+                    aria-describedby="helpId"
+                    placeholder="Type you name"
+                    bind:value={formdata.name}
+                />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+             </div>
+             <button on:click={(e) => {e.preventDefault();changeState('results');}}>Compute</button>
+         </form>
+         
+    {/if}
+</div>
